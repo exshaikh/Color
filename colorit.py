6ef74d66525c8deb2bed968fd50b3f9eeeee5271
exshaikh <muhammadaslamshaikh07@gmail.com>
2025-04-07 22:38:08 +0500

    Add files via upload

---
+++ colorit.py
@@ -0,0 +1,11 @@
+#  ColorIT    :  Color Trading Predictor Tool
+#  Version    :  v1.0
+#  Author     :  SomirIT Pvt. Ltd.
+#  GitHub     :  https://github.com/somirit
+#  E-Mail     :  info.somirit@gmail.com
+#
+#  Developer  :  Md. Nabid Ul Islam
+#
+#  Copyright  :  SomirIT Pvt. Ltd. (https://somirgc.com/somirit) 2025.
+
+import colorit
